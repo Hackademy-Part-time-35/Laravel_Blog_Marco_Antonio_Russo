aae06c85dc4592b9d47e43f5f73315a52534bf29
Marco Antonio Russo <Ocra94@gmail.com>
2024-06-28 10:13:18 +0200

    svolti esercizi extra

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -19,6 +20,14 @@ class ContactController extends Controller
 
         if($request->email){
             Mail::to("example@example.com")->send(new \App\Mail\ContactsEmail($request->name, $request->email, $request->content, $request->priority));
+
+            Contact::create([
+                "name"=> $request->name,
+                "email"=> $request->email,
+                "content"=> $request->content,
+                "priority" => $request->priority
+            ]);
+
             return redirect()->back()->with(["success" => "Inviato correttamente"]);
         }else{
             return redirect()->back()->with(["error" => "Compilare il campo \"EMAIL\""]);
